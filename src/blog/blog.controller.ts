30696ce62eef3aa8fa5102f2458fe8bf23a69d88
Arnoud Reinhardt Oscar Sarayar <rey.sarayar@gmail.com>
2024-01-31 23:10:07 +0700

    add get blog post by email and get individual post by id

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogDto } from './dto';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -14,5 +14,27 @@ export class BlogController {
   @ApiBearerAuth()
   async createBlog(@Body() dto: BlogDto, @Req() req: Request) {
     return this.blogService.createBlog(dto, req);
+  }
+
+  // get all blog by user loged in /blog/user-name/
+  @Get('allblog')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async getAllBlogByUserLogin(@Req() req: Request) {
+    return this.blogService.getAllBlogByUserLogin(req);
+  }
+
+  // get all blog by user but publicly
+  // blog/user/{email}
+  @Get('user/:email')
+  async getAllBlogByEmail(@Param('email') email: string) {
+    return this.blogService.getAllBlogByEmail(email);
+  }
+
+  // get blog by post id
+  // blog/post
+  @Get('post/:post_id')
+  async getBlogById(@Param('post_id') post_id: number) {
+    return this.blogService.getBlogById(post_id);
   }
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -5,10 +5,9 @@ import { BlogDto } from './dto';
 
 @Injectable()
 export class BlogService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async createBlog(dto: BlogDto, req: any) {
-    // console.log(req.user.sub);
     try {
       const user = req.user;
       const blog = await this.prisma.blog.create({
@@ -16,6 +15,52 @@ export class BlogService {
           title: dto.title,
           description: dto.description,
           userId: user.sub,
+        },
+      });
+      return blog;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllBlogByUserLogin(req: any) {
+    try {
+      const user = req.user.sub;
+      const allBlog = await this.prisma.blog.findMany({
+        where: {
+          userId: user,
+        },
+      });
+      return allBlog;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllBlogByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    const userId = user.id;
+    try {
+      const allBlogByEmail = await this.prisma.blog.findMany({
+        where: {
+          userId: userId,
+        },
+      });
+      return allBlogByEmail;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getBlogById(post_id: number) {
+    try {
+      const blog = await this.prisma.blog.findUnique({
+        where: {
+          id: Number(post_id),
         },
       });
       return blog;
